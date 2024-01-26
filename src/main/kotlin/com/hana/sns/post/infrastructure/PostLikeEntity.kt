715b0633a843f2ebb166a0dc2bf2ffb94052d5fa
hana0627 <hanana9506@naver.com>
2024-01-26 18:24:45 +0900

    PostLikeEntity 작성

--- src/main/kotlin/com/hana/sns/post/infrastructure/PostLikeEntity.kt
+++ src/main/kotlin/com/hana/sns/post/infrastructure/PostLikeEntity.kt
@@ -1,26 +1,20 @@
 package com.hana.sns.post.infrastructure
 
-import com.hana.sns.post.domain.Post
 import com.hana.sns.user.infrastructure.UserEntity
 import jakarta.persistence.*
-import org.hibernate.annotations.SQLDelete
-import org.hibernate.annotations.SQLRestriction
 import java.time.LocalDateTime
 
 @Entity
-@Table(name = "post")
-@SQLDelete(sql = "UPDATE post SET deleted_at = NOW(6) WHERE id=?")
-@SQLRestriction("deleted_at is NULL")
-class PostEntity(
-    @Column(name = "title")
-    val title: String,
-
-    @Column(name = "body", columnDefinition = "TEXT")
-    val body: String,
+@Table(name = "postLike")
+class PostLikeEntity (
 
     @ManyToOne
     @JoinColumn(name = "user_id", foreignKey = ForeignKey(name = "none", value = ConstraintMode.NO_CONSTRAINT))
     val user: UserEntity,
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", foreignKey = ForeignKey(name = "none", value = ConstraintMode.NO_CONSTRAINT))
+    val post: PostEntity,
 
     @Column(name = "registered_at")
     var registeredAt: LocalDateTime? = null,
@@ -28,19 +22,14 @@ class PostEntity(
     @Column(name = "updated_at")
     var updatedAt: LocalDateTime? = null,
 
-    @Column(name = "deleted_at")
-    var deletedAt: LocalDateTime? = null,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
-) {
 
-    constructor(post: Post) : this(
-        post.title,
-        post.body,
-        UserEntity(post.user),
-    )
+    ){
+
+
     @PrePersist
     fun registeredAt() {
         this.registeredAt = LocalDateTime.now();
@@ -50,5 +39,4 @@ class PostEntity(
     fun updatedAt() {
         this.updatedAt = LocalDateTime.now();
     }
-
 }
